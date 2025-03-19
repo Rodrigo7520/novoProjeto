505aba7060f73e5d726e059b5881480f946e1329
rodrigfontoura <rodrigofontoura06@gmail.com>
2025-03-19 11:10:57 -0300

    Ajustes Novos

--- src/contatos.js
+++ src/contatos.js
@@ -120,18 +120,23 @@ function Map() {
   );
 }
 
-// Componente Footer
+// Footer
 function Footer() {
   return (
     <footer>
       <nav>
         <ul>
           <li><Link to="/Home">Home</Link></li>
-          <li><Link to="/sobreNos">Sobre nós</Link></li>
+          <li><Link to="/sobreNos">Sobre Nós</Link></li>
           <li><Link to="/politicaLoja">Política da Loja</Link></li>
           <li><Link to="/contatos">Contato</Link></li>
         </ul>
       </nav>
+
+      <div className="footer-images">
+        <img src="https://static.wixstatic.com/media/e17308_10816183451746eb9c410e66a4111bd0~mv2.png/v1/fill/w_111,h_83,al_c,q_85,enc_avif,quality_auto/Site-Ronroninha-logo-catshop-rodape.png" alt="Imagem 1" />
+        <img src="https://static.wixstatic.com/media/e17308_f1256b17c4f748ba988b1f7975eee6be~mv2.png/v1/fill/w_164,h_83,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Site-Ronroninha-logo-catsitter-rodape2.png" alt="Imagem 2" />
+      </div>
 
       <div>
         <p>@ 2025 por Ronroninha Cat Shop CNPJ 37.848.640/0001-66</p>
